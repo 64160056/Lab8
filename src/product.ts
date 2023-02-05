4ebfa6c05575b78cada2fab8f7b76562a1f83f2d
Pasinee Rukpeomrach <64160056@go.buu.ac.th>
2023-02-05 20:17:06 +0700

    updatedProduct and finish typeorm

--- src/product.ts
+++ src/product.ts
@@ -5,16 +5,13 @@ AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
     const productsRepository = AppDataSource.getRepository(Product)
-    const product = new Product()
-    product.name = "ยำปูม้า"
-    product.price = 60
-    await productsRepository.save(product)
-    console.log("Saved a new user with id: " + product.id)
-
-    console.log("Loading users from the database...")
+    
     const products = await productsRepository.find()
     console.log("Loaded product: ", products)
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    const updatedProduct = await productsRepository.findOneBy({id: 1})
+    console.log(updatedProduct)
+    updatedProduct.price = 55
+    await productsRepository.save(updatedProduct)
 
 }).catch(error => console.log(error))
